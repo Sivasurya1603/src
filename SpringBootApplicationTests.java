649012a3ee59e95a3ccaee15e47809b3220b283f
sivasurya <suryasiva268@gmail.com>
2023-03-11 20:38:07 +0530

    first commit

---
+++ SpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package src;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
